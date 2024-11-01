ba040a32557c5aecfeb875c89b53d1ba24cd6be1
EslamMayo09 <eslammayo2022@gmail.com>
2024-11-01 18:31:04 +0200

    Extend 3 items from 560 to 720 instead of 700 in drag slider

--- js/main.js
+++ js/main.js
@@ -418,7 +418,7 @@ function dragSlider(options) {
             { breakpoint: 10, settings: { slidesToShow: 1, slidesToScroll: 1 }},
             { breakpoint: 360, settings: { slidesToShow: 2, slidesToScroll: 2 }},
             { breakpoint: 560, settings: { slidesToShow: 3, slidesToScroll: 3 }},
-            { breakpoint: 700, settings: { slidesToShow: 4, slidesToScroll: 4 }},
+            { breakpoint: 720, settings: { slidesToShow: 4, slidesToScroll: 4 }},
             { breakpoint: 1000, settings: { slidesToShow: 5, slidesToScroll: 5 }},
             { breakpoint: 1300, settings: { slidesToShow: 6, slidesToScroll: 6 }}
         ];
